ab61f346d880909f174741acc4d4b3221c2d7659
julien@macbook <julien.stechele@gmail.com>
2012-12-12 18:34:31 +0100

    Fix : ouvre l'accès au fichier .pid "à la suite"

--- lib/Sioux.pm
+++ lib/Sioux.pm
@@ -4,8 +4,8 @@ package Sioux;
 
 # J'écris dans un fichier .pid le processus d'un fils
 sub enregistrerPID {
-  open(FICHIERPID, ">lib/.pid");
-  print FICHIERPID $_;
+  open(FICHIERPID, ">>lib/.pid");
+  print FICHIERPID "$_[0]\n";
   close(FICHIERPID);
 }
 
