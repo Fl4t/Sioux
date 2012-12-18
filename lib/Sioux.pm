cc8b1d6ce1cadc307149a99f133a7b2194cce19e
julien@macbook <julien.stechele@gmail.com>
2012-12-18 17:02:14 +0100

    Ajout de la fonction contentPage

--- lib/Sioux.pm
+++ lib/Sioux.pm
@@ -4,6 +4,17 @@ use warnings; # warnings suplémentaires
 
 package Sioux;
 
+sub contentPage {
+  my $debut = '<html><head><title>';
+  my $milieu = '</title></head><body><h1>';
+  my $fin = '</h1></body></html>';
+  my @content;
+  return $debut.'200 OK'.$milieu.'The request is OK'.$fin if $_[0] eq '200';
+  return $debut.'403 Forbidden'.$milieu.'Access is forbidden to the requested page'.$fin if $_[0] eq '403';
+  return $debut.'404 Not Found'.$milieu.'The server can not find the requested page'.$fin if $_[0] eq '404';
+  return $debut.'505 HTTP Version Not Supported'.$milieu.'The server does not support the http protocol'.$fin if $_[0] eq '503';
+}
+
 sub enTete {
   my $header = "HTTP/1.0 $_[1]\n";
   $header .= "Content-Type: $_[0];";
